dbaf2f7c8d86476ce72878a59c2fdfa40bb697ee
zhaojiatao <2529827281@qq.com>
2020-09-05 10:54:21 +0800

    完成登录/注册页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,75 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
-  })
 
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
-  })
-
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
-  form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
-      }
-    }
-  })
-
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
+$(function () {
+    //点击去注册让登录页面隐藏
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show()
     })
-  })
+    // 点击登录让注册页面隐藏
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide()
+    })
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
-    e.preventDefault()
-    $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
+
+    //自定义表单验证
+    var form = layui.form;
+    form.verify({
+        //自定义一个密码验证
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        //验证密码确认框
+        //首先拿到注册页面的密码框内容，再和确认密码框的值比较
+        repwd: function (value) {
+            var psd = $('#psd').val();
+            if (psd !== value) {
+                return '两次密码不一致';
+            }
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
-      }
     })
-  })
+
+
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        //阻止表单的默认提交行为
+
+        e.preventDefault()
+        //发起ajax请求
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        };
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+
+            if (res.status !== 0) {
+
+                return layer.msg(res.message)
+            };
+            layer.msg('注册成功，请登录！');
+            // 模拟人的点击行为
+            $('#link_login').click()
+        })
+
+
+
+    })
+    //监听登录表单的提交事件
+    $('#form_login').on('submit', function (e) {
+        //阻止表单的默认提交行为
+        e.preventDefault();
+        var data = {
+            username: $('#form_login [name=username]').val(),
+            password: $('#form_login [name=password]').val()
+        };
+        //发起ajax请求
+        $.post('http://ajax.frontend.itheima.net/api/login', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg('登录失败！')
+            };
+            layer.msg('登录成功！');
+            //登录成功后把token 保存到本地
+            localStorage.setItem("token ", res.token);
+            //登录完成后跳转到主页面
+            location.href = '/index.html'
+        })
+    })
 })
